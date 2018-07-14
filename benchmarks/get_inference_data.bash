1e1dbc9bd102fa41854b37d24e593530c837a112
Constantin Pape <constantin.pape@iwr.uni-heidelberg.de>
2018-07-14 16:00:52 +0200

    separate download scripts

---
+++ benchmarks/get_inference_data.bash
@@ -0,0 +1,6 @@
+# download data for inference
+wget https://cremi.org/static/data/sample_A_padded_20160501.hdf
+
+# move to data folder
+mkdir -p data
+mv sample_A_padded_20160501.hdf data/
--- benchmarks/get_train_data.bash
+++ benchmarks/get_train_data.bash
@@ -1,8 +1,6 @@
 # download data for inference
-wget https://cremi.org/static/data/sample_A_padded_20160501.hdf
 wget https://cremi.org/static/data/sample_A_20160501.hdf
 
 # move to data folder
-mkdir data
-mv sample_A_padded_20160501.hdf data/
+mkdir -p data
 mv sample_A_20160501.hdf data/
